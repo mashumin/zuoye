48349886f7590169f7497859075490d443ca6a2d
msm <1345241789@qq.com>
2016-11-10 09:25:33 +0800

    this is edit lunbo

--- js/index.js
+++ js/index.js
@@ -17,11 +17,11 @@ $(function(){
         }
         $(".lunbo a").eq(nam).animate({width:"80%",height:"80%"}).css("zIndex",0);
         $(".lunbo a").eq(num).animate({left:0},function(){
-            $(".lunbo a").eq(nam).css({width:"100%",height:"100%",left:"100%"});
+            // console.log(3)
+            $(".lunbo a").eq(nam).css({left:"100%",width:"100%",height:"100%"});
             nam=num;
             flg=true;
             time=0;
-
         }).css("zIndex",1);
     }
     function move2(){
@@ -30,24 +30,26 @@ $(function(){
         if(bili>1){
             bili=1;
         }
-         wid=bili*100+"%";
+        wid=bili*100+"%";
         $(".active").css("width",0).eq(nam).css("width",wid);
         if(flg==false){
             $(".active").css("width","0");
         }
     }
 
-    var t1=setInterval(move1,3000);
-    var t2=setInterval(move2,50);
+    var t=setInterval(move1,3000);
+    var tt=setInterval(move2,50);
 
 
     $(window).blur(function(){
-        clearInterval(t1);
-        clearInterval(t2);
+        clearInterval(t);
+        clearInterval(tt);
+        console.log("blur")
     })
     $(window).focus(function(){
-        t1=setInterval(move1,3000)
-        t2=setInterval(move2,50)
+        t=setInterval(move1,3000)
+        tt=setInterval(move2,50)
+        console.log("focus")
     })
 
     $(".leftbut").click(function(){
@@ -55,7 +57,8 @@ $(function(){
         if(num==-1){
             num=2
         }
-        stop();
+        // alert("left")
+        stop()
     })
     $(".rightbut").click(function(){
         num++;
@@ -63,19 +66,19 @@ $(function(){
             num=0
         }
         stop();
+        // alert("right")
     })
 
     $(".lubut>div").click(function(){
         num=$(this).index(".lubut>div");
+        console.log(num)
         stop();
     })
 
     function stop(){
-        clearInterval(t1);
-        clearInterval(t2);
 
-        $(".lunbut>div").find(".active").css("width",0);
-        $(".lunbut>div").eq(num).find(".active").css("width","100%");
+        $(".lunbut>div").find(".active").css("width","100%");
+        $(".lunbo a").eq(num).css("left","0");
 
         if(num>nam){
             $(".lunbo a").eq(nam).animate({width:"80%",height:"80%"}).css("zIndex",0);
@@ -83,12 +86,16 @@ $(function(){
                 $(".lunbo a").eq(nam).css({width:"100%",height:"100%",left:"100%"});
                 nam=num;
             }).css("zIndex",1);
-        } else{
+            // console.log(1)
+        }else{
             $(".lunbo a").eq(nam).animate({left:"100%"}).css("zIndex",1);
-            $(".lunbo a").eq(num).css({width:"80%",height:"80%",left:"0"}).animate({width:"100%",height:"100%"},function(){
-                num=nam;
+            $(".lunbo a").eq(num).css({width:"80%",height:"80%",left:0}).animate({width:"100%",height:"100%"},function(){
+                nam=num;
             });
+            // console.log(2)
         }
+        clearInterval(t);
+        clearInterval(tt);
     }
 
 
